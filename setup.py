412625136b292186b2547c666e79ef093d1fe007
quanticchaos <luis@magar.mx>
2019-09-21 15:40:12 +0000

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 plugin_identifier = "webcamSB"
 plugin_package = "octoprint_webcamSB"
 plugin_name = "Sidebar Webcam"
-plugin_version = "0.1.4"
+plugin_version = "0.1.5"
 plugin_description = """Sidebar webcam stream viewer"""
 plugin_author = "Luis Magar Brunner in collaboration with jneilliii"
 plugin_author_email = "luis@magar.mx"
